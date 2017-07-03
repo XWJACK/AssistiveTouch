8130e5c897014cfa319bcc004c99cedd6e6d751c
Jack <xuwenjiejack@gmail.com>
2017-07-03 13:16:10 +0800

    Init project

---
+++ Sources/AssistiveTouch.h
@@ -0,0 +1,12 @@
+//
+//  AssistiveTouch.h
+//  AssistiveTouch
+//
+//  Created by Jack on 03/07/2017.
+//  Copyright © 2017 XWJACK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double AssistiveTouchVersionNumber;
+FOUNDATION_EXPORT const unsigned char AssistiveTouchVersionString[];
