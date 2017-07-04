2b01e8cb9de3eb3fefca4c5516ff5c4bfd59be0a
Jack <xuwenjiejack@gmail.com>
2017-07-04 12:59:39 +0800

    Add Example into AssistiveTouch.

---
+++ Example/iOS/iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Jack on 04/07/2017.
+//  Copyright © 2017 XWJACK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
